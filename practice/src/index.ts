c855000ad14cd56e25e5550cfe020d492236a1a0
murakamiii <jiw46.xe@gmail.com>
2022-05-05 07:22:29 +0900

    object

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,15 +1,1 @@
-let fb: string = "";
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        fb += "FizzBuzz ";
-    } else if (i % 3 === 0) {
-        fb += "Fizz ";
-    } else if (i % 5 === 0) {
-        fb += "Buzz ";
-    } else {
-        fb += `${i} `;
-    }
-}
-
-console.log(fb);
