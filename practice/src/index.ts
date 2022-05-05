733ea7f6e627230b0f2957aeefb1ae1c1a920d78
murakamiii <jiw46.xe@gmail.com>
2022-05-05 12:10:52 +0900

    optional property

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,10 +1,23 @@
 type FooBar = {
     foo: number;
     bar: string;
+    opt?: string;
 }
+
+interface IFooBar {
+    foo: number;
+    bar: string;
+}
+
 const obj: FooBar = {
     foo: 1,
     bar: "str"
+};
+
+const obj1: FooBar = {
+    foo: 1,
+    bar: "str",
+    opt: "opt"
 };
 
 const obj2 = {
