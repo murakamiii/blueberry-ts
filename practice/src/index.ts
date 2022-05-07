d12bad609d98d1e7674dde377bfbd7cf026e745b
murakamiii <jiw46.xe@gmail.com>
2022-05-08 03:56:07 +0900

    callback

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -7,3 +7,13 @@ hange("John");
 const divine = (name: string): void => console.log(`${name}さんは人間です。`);
 
 divine("John");
+
+type Person = {
+    name: string;
+}
+
+const people: Person[] = [ { name: "Gerd" }, { name: "Peter" } ];
+const getName = (person: Person): string => person.name;
+
+const names = people.map(getName);
+console.log(names);
