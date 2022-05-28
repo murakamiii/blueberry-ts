a2834073a357bf520d785655a58866f900623d8b
murakamiii <jiw46.xe@gmail.com>
2022-05-29 01:10:23 +0900

    controll flow

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -28,3 +28,20 @@ type Role = "seer" | "hunter";
 type Seer = {
     divine(person: Human): boolean
 }
+
+type SignType = "plus" | "minus";
+function signNumber(type: SignType): number {
+    return type === "plus" ? 1 : -1;
+}
+
+function numberWithSign(num: number, type: SignType | "none"): number {
+    if (type === "none") {
+        return 0;
+    } else {
+        return num * signNumber(type);
+    }
+}
+
+console.log(numberWithSign(5, "plus"));
+console.log(numberWithSign(5, "minus"));
+console.log(numberWithSign(5, "none"));
